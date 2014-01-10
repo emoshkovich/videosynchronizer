ad819e546202ed23428166189460b1a2f8fff9bb
gpatonay <patonayg@gmail.com>
2014-01-10 18:00:07 +0100

    youtube and video files now work together

--- pages/main.js
+++ pages/main.js
@@ -1,6 +1,7 @@
 var player='myvideo'
 
 function load( url ){
+    reset(); // youtube and normal player has not compatibility with each other
     var p;
     if( url.indexOf("youtu") != -1 ){
         p=videojs( player, { "techOrder": ["youtube"], "src":url } );
@@ -10,4 +11,11 @@ function load( url ){
         p.src( url );
     }
     p.pause();
+}
+
+function reset(){
+    // restore the original video tag
+    var v='<video id="myvideo" class="video-js vjs-default-skin" controls width="600" height="300"> </video>'
+    videojs( player ).dispose();
+    document.getElementById( "player" ).innerHTML=v;
 }
