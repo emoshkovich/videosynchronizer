af9e3882ae3c22200a071b39deefaa647431bde2
gpatonay <patonayg@gmail.com>
2014-01-10 12:44:57 +0100

    make the same player for youtube and local video

--- pages/main.js
+++ pages/main.js
@@ -1,7 +1,13 @@
 var player='myvideo'
 
 function load( url ){
-    var p=videojs( player );
-    p.src( url );
+    var p;
+    if( url.indexOf("youtu") != -1 ){
+        p=videojs( player, { "techOrder": ["youtube"], "src":url } );
+    }
+    else {
+        p=videojs( player );
+        p.src( url );
+    }
     p.pause();
 }
